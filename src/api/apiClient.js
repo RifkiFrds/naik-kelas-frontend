fe75fa199113764e22d14bf5045facbaacad92ae
rifkifrds <muhamadrifkifirdaus22@gmail.com>
2025-12-02 16:05:40 +0700

    fix: remove unnecessary blank lines in apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
-  baseURL: BASE_URL, 
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default apiClient;
-
-
